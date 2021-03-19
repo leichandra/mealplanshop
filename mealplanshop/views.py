74060aeb02be2e5c91a08a900fc62942ba312390
Leichandra <leichandra.truong@gmail.com>
2021-03-18 21:00:39 -0700

    dropdown phase

--- mealplanshop/views.py
+++ mealplanshop/views.py
@@ -10,6 +10,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import datetime
+import calendar as calendar2
 
 def index(request):
   # now = datetime.datetime.now()
@@ -21,7 +22,9 @@ def profile(request):
   return render(request, 'mealplanshop/profile.html')
 
 def calendar(request):
-  return render(request, 'mealplanshop/calendar.html')
+  myCal = calendar2.HTMLCalendar(calendar2.SUNDAY)
+  myMonth = myCal.formatmonth(2009, 7)
+  return render(request, 'mealplanshop/calendar.html', {"month": myMonth})
 
 def recipes(request):
   return render(request, 'mealplanshop/recipes.html')
